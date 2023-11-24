9a0c949240c6dc794619ee2e6e0e9ff3995dfe23
kimjaer <wofus0422@gmail.com>
2023-11-24 11:34:08 +0900

    console.log로 확인해보기

---
+++ nhncategory.js
@@ -0,0 +1,200 @@
+const nhncategory = [
+  {
+    gnbList: "회사소개",
+    class: "gnbList",
+    href: "https://www.nhn.com/",
+    dblock: true,
+    sub: [
+      {
+        gnbSubList: "회사소개",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "국내법인",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "해외법인",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "브랜드",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+    ],
+  },
+  {
+    gnbList: "서비스",
+    class: "gnbList",
+    href: "https://www.nhn.com/",
+    dblock: false,
+    sub: [
+      {
+        gnbSubList: "게임",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "결제·광고",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "커머스",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "기술",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "콘텐츠",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+    ],
+  },
+  {
+    gnbList: "홍보",
+    class: "gnbList",
+    href: "https://www.nhn.com/",
+    dblock: true,
+    sub: [
+      {
+        gnbSubList: "보도자료",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+    ],
+  },
+  {
+    gnbList: "투자정보",
+    class: "gnbList",
+    href: "https://www.nhn.com/",
+    dblock: true,
+    sub: [
+      {
+        gnbSubList: "공시정보",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "주가정보",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "재무제표",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "IR 행사",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "일반공고",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+    ],
+  },
+  {
+    gnbList: "ESG",
+    class: "gnbList",
+    href: "https://www.nhn.com/",
+    dblock: true,
+    sub: [
+      {
+        gnbSubList: "ESG 경영",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "환경",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "사회",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "지배구조",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "기업윤리",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+      {
+        gnbSubList: "보고서•정책",
+        class: "gnbSubList",
+        href: "https://www.nhn.com/",
+        dblock: true,
+      },
+    ],
+  },
+  {
+    gnbList: "CAREERS",
+    class: "gnbList",
+    href: "https://www.nhn.com/",
+    dblock: true,
+    sub: []
+  },
+  {
+    gnbList: "INSIDE NHN",
+    class: "gnbList",
+    href: "https://www.nhn.com/",
+    dblock: true,
+    sub: []
+  },
+]
+
+// var firstvar = 0;
+// var snvar = 1;
+// console.log(nhncategory[firstvar].sub[snvar].gnbSubList)
+
+
+// firstvar = 1;
+// snvar = 2;
+// console.log(nhncategory[firstvar].sub[snvar].gnbSubList)
+
+// navihtml(0, 0)
+// navihtml(1, 0)
+// navihtml(1, 2)
+
+// function navihtml(f, s) {
+//   console.log(nhncategory[f].sub[s].gnbSubList)
+// }
+
